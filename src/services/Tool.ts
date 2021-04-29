36cec1a8930bf070ed623c52a4c3286b94ef8156
rafael.fumegalli <rafaelxxc@gmail.com>
2021-04-29 19:57:10 -0300

    feat: Create register tool service & Use Yup to validate request body

--- src/services/Tool.ts
+++ src/services/Tool.ts
@@ -1,5 +1,8 @@
-import { Tool } from "../models/tool";
-import { ToolRepository } from "../repositories/Tool";
+import * as yup from 'yup';
+import { CreateToolRequest } from '../controllers/Tool';
+import { ApiError } from '../errors/ApiError';
+import { Tool } from '../models/tool';
+import { ToolRepository } from '../repositories/Tool';
 
 class ToolService {
   public static async findAll(tag: string | undefined): Promise<Tool[]> {
@@ -9,6 +12,23 @@ class ToolService {
 
     return ToolRepository.findAll();
   }
+
+  public static async create(toolToCreate: CreateToolRequest): Promise<Tool> {
+    const schema = yup.object().shape({
+      title: yup.string().required(),
+      link: yup.string().required(),
+      description: yup.string().required(),
+      tags: yup.array(yup.string()).required(),
+    });
+
+    try {
+      await schema.validate(toolToCreate, { abortEarly: false });
+    } catch (err) {
+      throw new ApiError(err, 400);
+    }
+
+    return ToolRepository.create(toolToCreate);
+  } 
 }
 
 export { ToolService };
