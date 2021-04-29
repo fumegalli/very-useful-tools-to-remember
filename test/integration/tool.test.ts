18e20b967ded534749d510deb59a9cfeb9ca73fa
rafael.fumegalli <rafaelxxc@gmail.com>
2021-04-29 19:13:10 -0300

    test: Implement tag filter test

--- test/integration/tool.test.ts
+++ test/integration/tool.test.ts
@@ -22,7 +22,24 @@ describe('Tool tests', () => {
 
       expect(response.status).toBe(200);
       expect(response.body[0]).toEqual(expect.objectContaining(anyTool));
-      expect(response.body[0].id).toEqual(expect.any(String));
+    });
+
+    it('should be able to return all tools containing the tag to filter', async () => {
+      const tagToFilter = 'node';
+      const toolWithNode: Tool = {
+        link: 'any_link',
+        tags: [tagToFilter, 'any_tag'],
+        title: 'any_title',
+        description: 'any_description',
+      };
+
+      await new Tool(toolWithNode).save();
+
+      const response = await request(app).get(`/tools?tag=${tagToFilter}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(1);
+      expect(response.body[0]).toEqual(expect.objectContaining(toolWithNode));
     });
   });
 });
