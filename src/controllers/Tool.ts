162d5afd0a11437eb19b2cf8da790c60ae71bb96
rafael.fumegalli <rafaelxxc@gmail.com>
2021-04-29 19:12:57 -0300

    feat: Implement tag filter

--- src/controllers/Tool.ts
+++ src/controllers/Tool.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import { ToolService } from '../services/Tool';
 
 class ToolController {
-  public static async findAll(_: Request, res: Response) {
-    const tools = await ToolService.findAll();
+  public static async findAll(req: Request, res: Response) {
+    const tools = await ToolService.findAll(req.query.tag as string | undefined);
 
     return res.status(200).json(tools);
   }
--- src/repositories/Tool.ts
+++ src/repositories/Tool.ts
@@ -4,6 +4,10 @@ class ToolRepository {
   public static async findAll(): Promise<Tool[]> {
     return Tool.find();
   }
+
+  public static async findByTag(tag: string): Promise<Tool[]> {
+    return Tool.find({ tags: tag });
+  }
 }
 
 export { ToolRepository };
