f5920cf88f422441fc9523b0534a4ae1d5d61de6
rafael.fumegalli <rafaelxxc@gmail.com>
2021-04-29 19:56:36 -0300

    feat: Create ApiError and middleware to handle errors

---
+++ src/errors/ApiError.ts
@@ -0,0 +1,12 @@
+class ApiError {
+  message: string;
+
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 500) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export { ApiError }
