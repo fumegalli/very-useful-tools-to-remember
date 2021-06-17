325c7438af9d2a0c2c25304ce1349683c2c8541b
rafael.fumegalli <rafaelxxc@gmail.com>
2021-06-17 20:21:32 -0300

    refact(server): Use port env var

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import {app} from './app';
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(
+    process.env.PORT,
+    () => console.log(`Server is running on port ${process.env.PORT}`),
+);
